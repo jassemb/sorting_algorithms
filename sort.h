e4b34f5822b738eca54ba7ecb7136eaf96c4074c
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 <benalijassem201@gmail.com>
2021-10-21 16:44:29 -0700

    lib file

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void swap(int *xp, int *yp);
+
+
+
+
+#endif

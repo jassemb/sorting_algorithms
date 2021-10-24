1ef6d0533b8648a0317b7cfd10a5fef0965fcec9
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 <benalijassem201@gmail.com>
2021-10-24 10:24:30 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,16 +21,18 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
-				print_array(array, size);
 			}
-
-		swap(&array[min_idx], &array[i]);
+		if (min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
 	}
 }

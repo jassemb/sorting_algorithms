1f74cfb3d2211d32727d92e7cb9cd46ff5b8e23f
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 <benalijassem201@gmail.com>
2021-10-24 10:46:13 -0700

    lib

--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
-#ifndef _SORT_H_
-#define _SORT_H_
-
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,17 +11,19 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+void swap_nodes(listint_t *curr, listint_t **list, listint_t *head);
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void swap(int *xp, int *yp);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void swap(int *xp, int *yp);
 #endif

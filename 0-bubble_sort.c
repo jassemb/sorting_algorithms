64ddf7ea7be4663a1c388d84f0074f8207b35640
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 <benalijassem201@gmail.com>
2021-10-24 10:37:06 -0700

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,15 +21,18 @@ void swap(int *xp, int *yp)
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i;
+	int sw = 1;
 
-	for (i = 0; i < size - 1; i++)
+	while (sw == 1)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		sw = 0;
+		for (i = 1; i < size; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i - 1] > array[i])
 			{
-				swap(&array[j], &array[j + 1]);
+				swap(&array[i], &array[i - 1]);
+				sw = 1;
 				print_array(array, size);
 			}
 		}
